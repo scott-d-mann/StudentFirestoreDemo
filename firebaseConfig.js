7cee3aac5e9580224bd4bee91ce04f752b454798
Scott Mann <s.mann@latrobe.edu.au>
2026-04-28 07:27:17 +1000

    clear out API keys

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // STUDENTS: Please replace the empty strings with your own Firebase configuration values. You can find these values in your Firebase project settings under the "General" tab. Make sure to keep these values secure and do not share them publicly.
 const firebaseConfig = {
-  apiKey: "AIzaSyCIGBzff33AKYPmnnQN7xDyYGgaCajONk0",
-  authDomain: "madauth-d7f8a.firebaseapp.com",
-  projectId: "madauth-d7f8a",
-  storageBucket: "madauth-d7f8a.firebasestorage.app",
-  messagingSenderId: "879521784619",
-  appId: "1:879521784619:web:2802f3de3aa2787c09db6e",
-  measurementId: "G-NPT51C37S7"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "" 
 };
 
 // Initialize Firebase
